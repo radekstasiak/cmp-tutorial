29fbbb25caa7aa318664a78aa5947a797a0642df
Radoslaw Stasiak <radoslawstasiak@pm.me>
2024-12-10 11:38:21 +0100

    add BookListItem

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -1,13 +1,15 @@
 package com.plcoding.bookpedia
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.booklist.components.BookListItem
 import com.plcoding.bookpedia.book.presentation.booklist.components.BookSearchBar
 
 @Preview
@@ -25,5 +27,26 @@ private fun BookSearchBarPreview() {
                 .fillMaxWidth()
         )
     }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun BookListItemPreview() {
+    BookListItem(
+        book = Book(
+            id = "1",
+            title = "Book title",
+            imageUrl = "",
+            authors = listOf(),
+            description = null,
+            languages = listOf(),
+            firstPublishYear = null,
+            averageRating = 4.6,
+            ratingCount = 3,
+            numPages = 10,
+            numEditions = 1
+        ),
+        onBookClick = {}
+    )
 }
 
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -6,7 +6,7 @@ import com.plcoding.bookpedia.book.presentation.booklist.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-
+@Preview
 fun App() {
     BookListScreenRoot(
         viewModel = remember { BookListViewModel() },
