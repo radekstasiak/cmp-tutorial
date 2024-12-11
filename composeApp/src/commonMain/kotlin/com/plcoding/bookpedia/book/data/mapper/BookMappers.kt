7563363edbec4975db9a924cdfc51d3c3ddf5ef1
Radoslaw Stasiak <radoslawstasiak@pm.me>
2024-12-11 13:07:01 +0100

    add data layer for book search

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/mapper/BookMappers.kt
@@ -0,0 +1,23 @@
+package com.plcoding.bookpedia.book.data.mapper
+
+import com.plcoding.bookpedia.book.data.dto.SearchedBookDto
+import com.plcoding.bookpedia.book.domain.Book
+
+fun SearchedBookDto.toBook(): Book =
+    Book(
+        id = this.id,
+        title = this.title,
+        imageUrl = if (this.coverKey != null) {
+            "https://covers.openlibrary.org/b/olid/${coverKey}-L.jpg"
+        } else {
+            "https://covers.openlibrary.org/b/olid/${coverAlternativeKey}-L.jpg"
+        },
+        authors = this.authorNames.orEmpty(),
+        description = null,
+        languages = this.languages.orEmpty(),
+        firstPublishYear = this.firstPublishYear.toString(),
+        averageRating = this.ratingsAverage,
+        ratingCount = this.ratingsCount ?: 0,
+        numPages = this.numPagesMedian,
+        numEditions = this.numEditions ?: 0
+    )
